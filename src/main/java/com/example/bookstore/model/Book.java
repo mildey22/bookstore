d40d4d36cbf359a428e97d5df3d9a38affc208d0
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-02-25 20:48:13 +0200

    fixed error

--- src/main/java/com/example/bookstore/model/Book.java
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -1,7 +1,5 @@
 package com.example.bookstore.model;
 
-import org.apache.poi.ss.formula.functions.Columns;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
