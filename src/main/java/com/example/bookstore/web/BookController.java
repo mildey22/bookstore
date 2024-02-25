fa373b065de0015870d4e275e9430f6cc1ee45be
Aatos Otronen <127504684+mildey22@users.noreply.github.com>
2024-02-25 22:47:23 +0200

    3.2 assignment finished

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,17 +1,25 @@
 package com.example.bookstore.web;
 
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.bookstore.model.BookRepository;
 
 @Controller
 public class BookController {
 
-@RequestMapping(value = "index", method=RequestMethod.GET)
-public String showIndex (Model model) {
-System.out.println("Index endpoint");
-    return "index";
+    @Autowired
+    private BookRepository repository;
+
+@RequestMapping("/booklist")
+    public String bookList(Model model){
+        model.addAttribute("books", repository.findAll());
+    return "booklist";
 }
 
 
